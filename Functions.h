5cc66d9d35203175ba2975ad85e4ba585c12e350
ethankuehler <ethankuehler@gmail.com>
2017-05-13 21:17:40 -0400

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -5,17 +5,8 @@
 #ifndef MAZESOVER3_0_FUNCTIONS_H
 #define MAZESOVER3_0_FUNCTIONS_H
 
-<<<<<<< HEAD
-
 extern "C" {
 int START_SOLVER(char fileLocation[]);
 };
 
 #endif  // MAZESOVER3_0_FUNCTIONS_H
-=======
-extern "C" {
-int START_SOLVER(char fileLocation[]);
-};
-
-#endif  // MAZESOVER3_0_FUNCTIONS_H
->>>>>>> origin/LIB
