bb21a6ddd89c5257cee3ee744901b3795d662075
ethankuehler <ethankuehler@gmail.com>
2017-05-13 22:19:40 -0400

    updated START_SOLVER function to return error cods on fail

--- Functions.cpp
+++ Functions.cpp
@@ -1,14 +1,20 @@
 #include "Solver.h"
 #include "string"
 extern "C" {
-  int START_SOLVER(char fileLocation[]) {
-    std::string file = fileLocation;
-    try {
-      Solver s(file);
-      s.start();
-    } catch (const std::exception &e) {
-      return 1;
-    }
-    return 0;
+int START_SOLVER(char fileLocation[]) {
+  std::string file = fileLocation;
+  try {
+    Solver s(file);
+    s.start();
+  } catch (const StartNotFoundException& e) {
+    return 1;
+  } catch (const EndNotFoundException& e) {
+    return 2;
+  } catch (const NoPathToEnd& e) {
+    return 3;
+  } catch (...) {
+    return 4;
   }
+  return 0;
+}
 }
