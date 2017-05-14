a289a0a376ad33b3012336853f040f1516b26172
ethankuehler <ethankuehler@gmail.com>
2017-05-13 21:10:05 -0400

    updating code

--- Functions.cpp
+++ Functions.cpp
@@ -1,22 +1,3 @@
-//
-// Created by ethankuehler on 5/9/17.
-//
-
-#include "Functions.h"
-<<<<<<< HEAD
-#include "Solver.h"
-#include "string" extern "C" {
-  int START_SOLVER(char fileLocation[]) {
-    std::string file = fileLocation;
-    try {
-      Solver s(file);
-      s.start();
-    } catch (const std::exception& e) {
-      return 1;
-    }
-    return 0;
-  }
-=======
 #include "Solver.h"
 #include "string" extern "C" {
   int START_SOLVER(char fileLocation[]) {
@@ -29,5 +10,4 @@
     }
     return 0;
   }
->>>>>>> origin/LIB
 }
