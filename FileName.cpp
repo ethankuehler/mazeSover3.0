a08ec847f47cd283079d99d63ff8992eadc6e086
ethankuehler <ethankuehler@gmail.com>
2017-05-13 21:10:47 -0400

    mroe

--- FileName.cpp
+++ FileName.cpp
@@ -1,19 +1,8 @@
 #include "FileName.h"
-<<<<<<< HEAD
 #include <sstream>
-
-std::string getFileName()
-{
-  ++id;
-  std::ostringstream oss;
-  oss << "output" << id << ".bmp";
-  return oss.str();
-=======
-
 std::string getFileName() {
   ++id;
   std::ostringstream oss;
   oss << "output" << id << ".bmp";
   return oss.str();
->>>>>>> origin/LIB
 }
